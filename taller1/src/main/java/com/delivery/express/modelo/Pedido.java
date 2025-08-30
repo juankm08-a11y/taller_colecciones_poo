301eea496915f8f0159064393cf1b32e2dc64784
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-08-30 14:53:05 -0500

    create pedidos.java, colecciones con ejercicios.java and utilidades.java

---
+++ taller1/src/main/java/com/delivery/express/modelo/Pedido.java
@@ -0,0 +1,33 @@
+package com.delivery.express.modelo;
+
+import java.lang.Override;
+
+public class Pedido {
+    private String cliente;
+    private String producto;
+    private double total;
+
+    public Pedido(String cliente, String producto, double total) {
+        this.cliente = cliente;
+        this.producto = producto;
+        this.total = total;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return cliente + " " + producto + "($" + total + ")";
+    }
+
+}
